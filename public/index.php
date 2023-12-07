35dc030c1ab5b32f83d74d401f93edeb65f4a972
dipanjang7286 <71178502+dipanjang7286@users.noreply.github.com>
2023-12-07 23:20:12 +0530

    code added

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+use app\core\Application;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$app = new Application(dirname(__DIR__));
+$app->router->get("/", 'home');
+
+$app->router->get("/contact", 'contact');
+
+$app->router->post("/contact", function () {
+    return 'handle Submitted Data';
+});
+$app->run();
+?>
